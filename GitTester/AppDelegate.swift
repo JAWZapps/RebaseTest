5672c7a83dc45515782cb481f9c217b042dfbe97
JAWZapps <apps@jawz.co.uk>
2015-09-13 16:08:43 +0100

    added some more develop crap

--- GitTester/AppDelegate.swift
+++ GitTester/AppDelegate.swift
@@ -52,6 +52,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDele
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
         // Saves changes in the application's managed object context before the application terminates.
         self.saveContext()
+        
+        println("this is some more develop crap")
     }
 
     // MARK: - Split view
