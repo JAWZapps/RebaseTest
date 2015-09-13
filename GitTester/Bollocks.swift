4b3634c353d2182cfd55a06eef8b9a3acdebb071
JAWZapps <apps@jawz.co.uk>
2015-09-13 17:41:11 +0100

    some develop Bollocks

---
+++ GitTester/Bollocks.swift
@@ -0,0 +1,15 @@
+//
+//  Bollocks.swift
+//  GitTester
+//
+//  Created by Me on 13/09/2015.
+//  Copyright (c) 2015 JAWZ. All rights reserved.
+//
+
+import Foundation
+
+class Bollocks {
+    init() {
+        println("this is some development Bollocks")
+    }
+}
